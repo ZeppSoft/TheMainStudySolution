2626a27cf07c6fae9e3e6794dc50bc7d9086c41d
Viktor Shershnov <viktor.shershnov@quipu.de>
2019-01-09 11:10:25 +0200

    Added project Recursion

---
+++ Recursion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        public static void Recursion(int number)
+        {
+            number--;
+
+            Console.WriteLine($"First part of app, number = {number}");
+
+            if (number > 0)
+                Recursion(number);
+
+            Console.WriteLine($"Second part of app, number = {number}");
+        }
+        static void Main(string[] args)
+        {
+            Recursion(2);
+        }
+    }
+}
